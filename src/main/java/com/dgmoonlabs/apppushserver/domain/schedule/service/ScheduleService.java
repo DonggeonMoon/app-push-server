4df5309c8181d5adb48637ebd29d434c5a5a06fe
Donggeon Moon <nullsafer@naver.com>
2023-09-30 19:29:09 +0900

    feat: 일정 서비스 작성

---
+++ src/main/java/com/dgmoonlabs/apppushserver/domain/schedule/service/ScheduleService.java
@@ -0,0 +1,32 @@
+package com.dgmoonlabs.apppushserver.domain.schedule.service;
+
+import com.dgmoonlabs.apppushserver.domain.schedule.model.Schedule;
+import com.dgmoonlabs.apppushserver.domain.schedule.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
+
+    public List<Schedule> getSchedulesOfToday() {
+        return scheduleRepository.findByDate(LocalDate.now());
+    }
+
+    public List<Schedule> insertSchedules(List<Schedule> schedules) {
+        return scheduleRepository.insert(schedules);
+    }
+
+
+    public void deleteSchedules(List<Schedule> schedules) {
+        scheduleRepository.deleteAll(schedules);
+    }
+
+    public void deleteAllSchedules() {
+        scheduleRepository.deleteAll();
+    }
+}
