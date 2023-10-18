78fc897cad8804e6510773392bbca2386f48e53d
Donggeon Moon <nullsafer@naver.com>
2023-10-18 21:06:24 +0900

    feat: Firebase 인증 추가

--- src/test/java/com/dgmoonlabs/apppushserver/AppPushServerApplicationTests.java
+++ src/test/java/com/dgmoonlabs/apppushserver/AppPushServerApplicationTests.java
@@ -1,6 +1,8 @@
 package com.dgmoonlabs.apppushserver;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
@@ -8,14 +10,23 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 @SpringBootTest
 @Slf4j
 class AppPushServerApplicationTests {
+    private String topic = "STOCK_CALENDAR";
 
     @Test
-    void test() throws FirebaseMessagingException {
-        String registrationToken = "token";
-        FirebaseApp.initializeApp();
+    void test() throws FirebaseMessagingException, IOException {
+        FileInputStream fileInputStream = new FileInputStream("./app-push-3457c-firebase-adminsdk-j66ek-fd98d0d8ba.json");
+
+        FirebaseOptions firebaseOptions = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(fileInputStream))
+                .build();
+        FirebaseApp.initializeApp(firebaseOptions);
+
         Message message = Message.builder()
                 .putData("score", "850")
                 .putData("time", "2:45")
