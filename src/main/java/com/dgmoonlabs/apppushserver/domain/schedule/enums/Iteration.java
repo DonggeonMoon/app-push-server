272ea466ca3a12daff439e7de7181aee83b3fa64
Donggeon Moon <nullsafer@naver.com>
2023-10-23 15:00:48 +0900

    feat: JPA 엔티티 작성

---
+++ src/main/java/com/dgmoonlabs/apppushserver/domain/schedule/enums/Iteration.java
@@ -0,0 +1,8 @@
+package com.dgmoonlabs.apppushserver.domain.schedule.enums;
+
+public enum Iteration {
+    ONCE,
+    YEARLY,
+    WEEKLY,
+    DAILY
+}
--- src/main/java/com/dgmoonlabs/apppushserver/domain/schedule/model/Schedule.java
+++ src/main/java/com/dgmoonlabs/apppushserver/domain/schedule/model/Schedule.java
@@ -1,22 +1,40 @@
 package com.dgmoonlabs.apppushserver.domain.schedule.model;
 
+import com.dgmoonlabs.apppushserver.domain.schedule.enums.Iteration;
+import com.google.firebase.messaging.AndroidConfig;
+import com.google.firebase.messaging.ApnsConfig;
+import com.google.firebase.messaging.WebpushConfig;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Document(collection = "schedule")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
 @Getter
-@EqualsAndHashCode
-public class Schedule {
+public class Schedule extends BaseEntity {
     @Id
     private String id;
 
-    private LocalDate date;
+    private LocalDateTime dateTime;
+
+    private Iteration iteration;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    private String title;
 
     private String description;
+
+    private AndroidConfig androidConfig;
+
+    private ApnsConfig apnsConfig;
+
+    private WebpushConfig webpushConfig;
 }
