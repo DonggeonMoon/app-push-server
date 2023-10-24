1a006ef9d313db2908686c7b4fd0c3ddc7f1a152
Donggeon Moon <nullsafer@naver.com>
2023-10-24 10:52:29 +0900

    feat: firebase 토픽 enum 작성

---
+++ src/main/java/com/dgmoonlabs/apppushserver/global/enums/TopicName.java
@@ -0,0 +1,12 @@
+package com.dgmoonlabs.apppushserver.global.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum TopicName {
+    ALL_USERS("STOCK_CALENDAR_ALL_USERS");
+
+    private final String value;
+}
