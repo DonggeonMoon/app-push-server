3922c2c983d1f94ae38de6d89951d1949731e32b
Donggeon Moon <nullsafer@naver.com>
2024-08-06 05:56:38 +0900

    feat: jpa 감사 기능

---
+++ src/main/java/com/dgmoonlabs/apppushserver/global/model/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.dgmoonlabs.apppushserver.global.model;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+    private LocalDateTime deletedAt;
+}
