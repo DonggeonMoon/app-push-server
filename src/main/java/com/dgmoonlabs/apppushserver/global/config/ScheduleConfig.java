1fbf51324b077be179ac1dcbd7324b13afc4276b
Donggeon Moon <nullsafer@naver.com>
2024-08-06 05:57:13 +0900

    feat: 스케쥴러 설정

---
+++ src/main/java/com/dgmoonlabs/apppushserver/global/config/ScheduleConfig.java
@@ -0,0 +1,9 @@
+package com.dgmoonlabs.apppushserver.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class ScheduleConfig {
+}
